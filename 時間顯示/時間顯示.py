d31966f9f95c40338f319aa09e784d999b8d913a
user <user@WEICHENG-LAPTOP>
2023-02-18 00:24:14 +0800

    加入專案檔案。

---
+++ 時間顯示/時間顯示.py
@@ -0,0 +1,16 @@
+import time
+from turtle import*
+import turtle
+while 1==1:
+           import turtle
+           turtle.hideturtle
+           turtle.goto(-300,-40)
+           turtle.clear()
+           now=time.ctime()
+           now=str(now)
+           turtle.write(now,font=("Arial",36,"bold"))
+           time.sleep(1)
+           turtle.clear()
+           
+           
+
